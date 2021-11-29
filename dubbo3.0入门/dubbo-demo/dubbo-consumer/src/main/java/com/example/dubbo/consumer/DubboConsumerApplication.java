f02ab8a766926520c5d4033a1059c23107e16814
wangxw257 <wangxw257@chinaunicom.cn>
2021-11-29 11:56:38 +0800

    springboot集成caffeine

--- dubbo3.0入门/dubbo-demo/dubbo-consumer/src/main/java/com/example/dubbo/consumer/DubboConsumerApplication.java
+++ dubbo3.0入门/dubbo-demo/dubbo-consumer/src/main/java/com/example/dubbo/consumer/DubboConsumerApplication.java
@@ -2,7 +2,9 @@ package com.example.dubbo.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource(locations = {"classpath:applicationContext-dubbo.xml"})
 @SpringBootApplication
 public class DubboConsumerApplication {
 
